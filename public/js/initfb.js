b6f13875414f4856bdf8b535ad9b3a87fb5d1703
zachanator070 <zach.johnson070@gmail.com>
2016-12-05 23:19:23 -0700

    changing check login

--- public/js/initfb.js
+++ public/js/initfb.js
@@ -1,23 +1,41 @@
-window.fbAsyncInit = function() {
-  FB.init({
-    appId      : '359867534367940',
-    xfbml      : true,
-    version    : 'v2.6'
-  });
-  checkLogin();
-};
 
-(function(d, s, id){
-   var js, fjs = d.getElementsByTagName(s)[0];
-   if (d.getElementById(id)) {return;}
-   js = d.createElement(s); js.id = id;
-   js.src = "//connect.facebook.net/en_US/sdk.js";
-   fjs.parentNode.insertBefore(js, fjs);
- }(document, 'script', 'facebook-jssdk'));
+$.ready(function(){
+  window.fbAsyncInit = function() {
+    FB.init({
+      appId      : '359867534367940',
+      xfbml      : true,
+      version    : 'v2.6'
+    });
+    checkLogin();
+  };
 
+  (function(d, s, id){
+     var js, fjs = d.getElementsByTagName(s)[0];
+     if (d.getElementById(id)) {return;}
+     js = d.createElement(s); js.id = id;
+     js.src = "//connect.facebook.net/en_US/sdk.js";
+     fjs.parentNode.insertBefore(js, fjs);
+   }(document, 'script', 'facebook-jssdk'));
+
+});
 
 function checkLogin(){
   FB.getLoginStatus(function(response) {
-    console.log(response);
+
+    // logged in but not authorized
+    if(response.status === "not_authorized"){
+
+    }
+
+    // logged in and authorized
+    if(response.status === "connected"){
+      $('postbutton').removeClass('disabled');
+      $('postbutton').text("Post Something Random");
+    }
+
+    // not logged in
+    if(response.status === "unknown"){
+
+    }
   });
 }
